4a660edb7b7654e8d0873d5d741c42464d4f0409
cincy-coder <junocincy@gmail.com>
2025-02-12 15:29:19 +0530

    settings.py

--- Hmart/settings.py
+++ Hmart/settings.py
@@ -52,11 +52,7 @@ INSTALLED_APPS = [
     'admin_app',
     'cart_app',
     
-    # Allauth apps
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+
 ]
 
 CART_SESSION_ID = 'cart'
@@ -122,20 +118,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+
 
 
 # Internationalization
